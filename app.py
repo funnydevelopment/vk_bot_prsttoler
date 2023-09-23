dd6d4514210617f24f3471dababd5e8a7fbecd0b
funnydevelopment <ya.giggs2013@yandex.ru>
2023-09-23 05:22:28 +0300

    updated create post

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from dotenv import load_dotenv
 
 from core.views import create_post
+from core.data import get_group_id
 
 
 def run_app():
     load_dotenv(".env")
-    create_post()
+    vk_groups_id = get_group_id()
+    for vk_group_id in vk_groups_id:
+        create_post(vk_group_id)
 
 
 if __name__ == "__main__":
--- core/views.py
+++ core/views.py
@@ -1,25 +1,31 @@
 import os
+import time
 
 import httpx
 
 from core import texts
 
 
-def create_post() -> None:
-    access_token = os.getenv("ACCESS_TOKEN")
-    owner_id = int(os.getenv("OWNER_ID"))
-    from_group = 0
-    message = texts.MESSAGE_TEXT
-    version = "5.131"
-    params = {
-        "access_token": access_token,
-        "owner_id": owner_id,
-        "from_group": from_group,
-        "message": message,
-        "v": version,
-    }
-    url = "https://api.vk.com/method/wall.post"
-    code = os.getenv("CODE")
-    response = httpx.post(url, params=params)
-    result = response.json()
-    print(result)
+def create_post(vk_group_id: int) -> None:
+    try:
+        access_token = os.getenv("ACCESS_TOKEN")
+        owner_id = vk_group_id
+        from_group = 0
+        message = texts.MESSAGE_TEXT
+        attachment = ""
+        version = "5.131"
+        params = {
+            "access_token": access_token,
+            "owner_id": owner_id,
+            "from_group": from_group,
+            "message": message,
+            "attachments": attachment,
+            "v": version,
+        }
+        url = "https://api.vk.com/method/wall.post"
+        response = httpx.post(url, params=params)
+        result = response.json()
+        print(result)
+    except Exception as error:
+        print(f"Произошла ошибка {error}")
+    time.sleep(60)
